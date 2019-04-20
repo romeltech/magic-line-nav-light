16b55cd282580934c4ef10c571f2b679769e2e84
mel <indemnedev@gmail.com>
2019-04-20 16:51:21 +0400

    added color picker

--- magic-line-nav-light.php
+++ magic-line-nav-light.php
@@ -19,8 +19,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // define('WP_DEBUG', true);
 
-define( 'MAGIC_LINE_LIGHT_VERSION', '0.0.1' ); 
-define( 'MAGIC_LINE_LIGHT_AUTHOR', 'Romel Indemne' );
+define( 'MAGIC_LINE_NAV_LIGHT_VERSION', '0.0.1' ); 
+define( 'MAGIC_LINE_NAV_LIGHT_AUTHOR', 'Romel Indemne' );
 
 if(! class_exists('MagicLineNavigationLight') ){
 
@@ -35,6 +35,7 @@ if(! class_exists('MagicLineNavigationLight') ){
             add_action( 'admin_menu', array( $this, 'create_plugin_settings_page' ) );
             add_action( 'admin_init', array( $this, 'setup_sections' ) );
             add_action( 'admin_init', array( $this, 'setup_fields' ) );
+            add_action('admin_enqueue_scripts', array( $this, 'ml_enqueue_scripts' ) );
         }
 
         /**
@@ -51,13 +52,21 @@ if(! class_exists('MagicLineNavigationLight') ){
 
         }
     
+        /**
+         * Enqueue Scripts
+         */
+        public function ml_enqueue_scripts() { 
+     
+            // Make sure to add the wp-color-picker dependecy to js file
+            wp_enqueue_script( 'cpa_custom_js', plugins_url( '/assets/js/ml-admin.js', __FILE__ ), array( 'jquery', 'wp-color-picker' ), '', true  );
+        }
 
 
         /**
          * settings_link
          */
         public function settings_link($links){
-            $settings_link = '<a href="admin.php?page=magic_line_light">Settings</a>';
+            $settings_link = '<a href="admin.php?page=magic_line_nav_light">Settings</a>';
             $documentation_link = '<a href="https://mel-7.com/" target="__blank">Documentation</a>';
             array_push($links, $settings_link, $documentation_link);
             return $links;
@@ -72,7 +81,7 @@ if(! class_exists('MagicLineNavigationLight') ){
             $page_title = 'Magic Line Navigation';
             $menu_title = 'Magic Line';
             $capability = 'manage_options';
-            $slug = 'magic_line_light';
+            $slug = 'magic_line_nav_light';
             $callback = array( $this, 'plugin_settings_page_content' );
             $icon = 'dashicons-editor-underline';
             $position = 100;
@@ -89,8 +98,8 @@ if(! class_exists('MagicLineNavigationLight') ){
                 <?php settings_errors(); ?>
                 <form method="post" action="options.php">
                     <?php
-                        settings_fields( 'magic_line_light' );
-                        do_settings_sections( 'magic_line_light' );
+                        settings_fields( 'magic_line_nav_light' );
+                        do_settings_sections( 'magic_line_nav_light' );
                         submit_button();
                     ?>
                 </form>
@@ -98,18 +107,20 @@ if(! class_exists('MagicLineNavigationLight') ){
         }
 
         public function setup_sections() {
-            add_settings_section( 'our_first_section', 'Setup', array( $this, 'section_callback' ), 'magic_line_light' );
+            add_settings_section( 'magic_line_nav_color', '', array( $this, 'section_callback' ), 'magic_line_nav_light' );
+            add_settings_section( 'magic_line_nav_height', '', array( $this, 'section_callback' ), 'magic_line_nav_light' );
+            // add_settings_section( 'magic_line_nav_width', 'Width', array( $this, 'section_callback' ), 'magic_line_nav_light' );
         }
 
         public function section_callback( $arguments ) {
             switch( $arguments['id'] ){
-                case 'our_first_section':
-                    echo 'Here you can add the navigation you want to have a magic line effect';
+                case 'magic_line_nav_color':
+                    // echo 'Set the Magicline color';
                     break;
-                // case 'our_second_section':
-                //     echo 'This one is number two';
-                //     break;
-                // case 'our_third_section':
+                case 'magic_line_nav_height':
+                    // echo 'Here you can add the navigation you want to have a magic line effect';
+                    break;
+                // case 'magic_line_nav_width':
                 //     echo 'Third time is the charm!';
                 //     break;
             }
@@ -118,22 +129,43 @@ if(! class_exists('MagicLineNavigationLight') ){
         public function setup_fields() {
             $fields = array(
                 array(
-                    'uid' => 'mll_nav_selector',
-                    'label' => 'Navigation Selector',
-                    'section' => 'our_first_section',
+                    'uid' => 'ml_color',
+                    'label' => 'Color',
+                    'section' => 'magic_line_nav_color',
+                    'type' => 'text',
+                    'options' => false,
+                    'placeholder' => null,
+                    'helper' => null,
+                    'supplemental' => 'Set the color',
+                    'default' => '#fe4902'
+                ),
+                array(
+                    'uid' => 'ml_height',
+                    'label' => 'Height',
+                    'section' => 'magic_line_nav_height',
                     'type' => 'text',
                     'options' => false,
                     'placeholder' => 'Add your Navigation Selector',
                     'helper' => null,
-                    'supplemental' => null,
-                    'default' => 'ul.header-nav li.menu-itm a.nav-top-link'
+                    'supplemental' => 'Input the height in px e.g. 2px',
+                    'default' => '2px'
                 )
             );
             foreach( $fields as $field ){
-                add_settings_field( $field['uid'], $field['label'], array( $this, 'field_callback' ), 'magic_line_light', $field['section'], $field );
-                register_setting( 'magic_line_light', $field['uid'] );
+                 // id, title, display cb, page, section
+                // add_settings_field( 'magic_line_nav_color', 'Background Color', array( $this, 'ml_color_picker_field' ), __FILE__, 'magic_line_nav_light' );
+                // add_settings_field( $field['uid'], $field['label'], array( $this, 'field_callback' ), 'magic_line_nav_light', $field['section'], $field  );
+                add_settings_field( $field['uid'], $field['label'], array( $this, 'field_callback' ), 'magic_line_nav_light', $field['section'], $field );
+                register_setting( 'magic_line_nav_light', $field['uid'] );
             }
         }
+
+        // Color Picker Callback Function
+        // public function ml_color_picker_field(){
+        //     // $val = ( isset( $this->options['title'] ) ) ? $this->options['background'] : '';
+        //     // echo '<input type="text" name="cpa_settings_options[background]" value="' . $val . '" class="cpa-color-picker" >';
+        //     echo '<input type="text" name="magic_line_nav_color" value="" class="ml-color-field" >';
+        // }
 
 
         public function field_callback( $arguments ) {
@@ -142,12 +174,45 @@ if(! class_exists('MagicLineNavigationLight') ){
                 $value = $arguments['default']; // Set to our default
             }
         
+          
+
             // Check which type of field we want
             switch( $arguments['type'] ){
                 case 'text': // If it is a text field
-                    printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" style="width:400px;"/>', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
+                    if($arguments['section'] == 'magic_line_nav_color'){
+                        // printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" class="ml-color-field" style="width:400px;"/>', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
+                        printf( '<input name="%1$s" id="%1$s" type="%2$s" value="%4$s" class="ml-color-field" >', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
+                    }else{
+
+                        printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" style="width:400px;"/>', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
+                    }
                     break;
+
+                // case 'textarea': // If it is a textarea
+                //     printf( '<textarea name="%1$s" id="%1$s" placeholder="%2$s" rows="5" cols="50">%3$s</textarea>', $arguments['uid'], $arguments['placeholder'], $value );
+                // break;
+                
+                // case 'select': // If it is a select dropdown
+                // if( ! empty ( $arguments['options'] ) && is_array( $arguments['options'] ) ){
+                //     $options_markup = ’;
+                //     foreach( $arguments['options'] as $key => $label ){
+                //         $options_markup .= sprintf( '<option value="%s" %s>%s</option>', $key, selected( $value, $key, false ), $label );
+                //     }
+                //     printf( '<select name="%1$s" id="%1$s">%2$s</select>', $arguments['uid'], $options_markup );
+                // }
+                // break;
             }
+
+            // If there is help text
+            if( $helper = $arguments['helper'] ){
+                printf( '<span class="helper"> %s</span>', $helper ); // Show it
+            }
+
+            // If there is supplemental text
+            if( $supplimental = $arguments['supplemental'] ){
+                printf( '<p class="description">%s</p>', $supplimental ); // Show it
+            }
+
         }
     }
 
@@ -160,7 +225,12 @@ if(! class_exists('MagicLineNavigationLight') ){
      */
     function magic_line_nav_light_script() {
 
-        $mll_selector = get_option('mll_nav_selector'); ?>
+        $ml_height = get_option('ml_height'); 
+        
+       
+        
+        ?>
+
 
         <!-- Magic Line Navigation -->
         <?php /*
@@ -172,8 +242,9 @@ if(! class_exists('MagicLineNavigationLight') ){
         <style>
         .magic-line-nav-parent{ position: relative;}
         .magic-line-nav-parent li { display: inline-flex; }
-        #magic-line{ position: absolute; bottom: -2px; left: 0;height: 2px; background: #fe4902; margin: 0 auto !important; }
-        .magic-line-nav-parent li ul{ top: 100%;}</style>
+        #magic-line{ position: absolute; bottom: -2px; left: 0;height:<?php echo $ml_height; ?>; background: #fe4902; margin: 0 auto !important; }
+        .magic-line-nav-parent li ul{ top: 100%;}
+        @media all and (max-width: 900px) {#magic-line{display:none;}}</style>
         <script>
             (function($){
                 'use strict';
@@ -216,6 +287,7 @@ if(! class_exists('MagicLineNavigationLight') ){
                     $magicLine.stop().animate({
                         left: leftPos,
                         width: newWidth
+                // }, 1000, 'swing');
                 });
                 }, function() {
                     $magicLine.stop().animate({
